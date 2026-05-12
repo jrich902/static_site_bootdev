1fdd96924517873fd9a3c084dca7a171f587464a
jrich902 <justin.c.richards@outlook.com>
2026-05-12 00:05:15 -0300

    adding leaf node and unit tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -20,4 +20,20 @@ class HTMLNode:
             return result                
         else: 
             return ""
-    
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag=None, value=None, props=None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("No Value found! All leaf nodes must have a value")
+        
+        if not self.tag:
+            return self.value
+        
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})" 
+
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -10,18 +10,29 @@ class TestHTMLNode(unittest.TestCase):
     def test_one_prop(self):
         node = HTMLNode("p", "hello", None, {"class": "greeting"})
         result = node.props_to_html()
-        print(node.props_to_html)
         node2 = HTMLNode("p", "hello", None, {"class": "greeting"})
         result2 = node2.props_to_html()
         self.assertEqual(result, result2)
-        
-    def test_two_prop(self):
+
+    def test_two_props(self):
         node = HTMLNode("p", "hello", None, {"class": "greeting", "id": "main"})
         result = node.props_to_html()
-        print(node.props_to_html)
         node2 = HTMLNode("p", "hello", None, {"class": "greeting", "id": "main"})
         result2 = node2.props_to_html()
         self.assertEqual(result, result2)
+
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_no_value(self):
+        node = LeafNode("p", None)
+        with self.assertRaises(ValueError):
+            node.to_html()
+
+    def test_leaf_two_props(self):
+        node = LeafNode("p", "hello", {"class": "greeting", "id": "main"})
+        self.assertEqual(node.to_html(), '<p class="greeting" id="main">hello</p>')
 
 if __name__ == "__main__":
     unittest.main()
